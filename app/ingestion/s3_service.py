33ef528b1329786494c036758e2885f1319f6cd3
Jayachadnra-v77 <jayachandra.v986@gmail.com>
2026-07-28 10:21:01 +0530

    pdfs temp download

--- app/ingestion/s3_service.py
+++ app/ingestion/s3_service.py
@@ -28,11 +28,11 @@ class S3Service:
 
         return pdf_files
 
-    def download_pdf(self, s3_key, local_folder="data/pdfs"):
+    def download_pdf(self, s3_key, local_folder="data/temp_pdfs"):
 
-        Path(local_folder).mkdir(parents=True, exist_ok=True)
+        local_path = Path(local_folder) / Path(s3_key)
 
-        local_path = Path(local_folder) / Path(s3_key).name
+        local_path.parent.mkdir(parents=True, exist_ok=True)
 
         self.client.download_file(
             self.bucket,
@@ -42,17 +42,39 @@ class S3Service:
 
         return str(local_path)
 
+    def delete_local_pdf(self, local_path):
+
+        file_path = Path(local_path)
+
+        if file_path.exists():
+            file_path.unlink()
 
 
 if __name__ == "__main__":
 
     service = S3Service()
 
-    print("Bucket:", service.bucket)
+    print(f"\nBucket : {service.bucket}")
 
     pdf_files = service.list_pdf_files()
 
-    print(f"Total PDF Files: {len(pdf_files)}")
+    print(f"Total PDF Files : {len(pdf_files)}\n")
 
-    for pdf in pdf_files:
-        print(pdf)
+    # Download only one file at a time
+    for index, s3_key in enumerate(pdf_files, start=1):
+
+        print(f"[{index}] Processing : {s3_key}")
+
+        # Download PDF
+        local_pdf = service.download_pdf(s3_key)
+
+        country = Path(s3_key).parts[0]
+
+        print(f"Country      : {country}")
+        print(f"Local File   : {local_pdf}")
+
+        # Stop here.
+        # Next step is:
+        # PDFLoader -> ChunkService -> EmbeddingService -> PineconeService
+
+        break
